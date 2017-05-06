b76a4e6cf3dc97aa7a363fc675c45457683834bd
Kenney <zql254@vip.qq.com>
2017-05-06 13:35:36 +0800

    fix

--- config.json.js
+++ config.json.js
@@ -1,11 +1,11 @@
 {
-    "build_date": "2017-05-06 12:39:59", 
+    "build_date": "2017-05-06 13:35:24", 
     "description": "多终端同步工具", 
     "home_url": "Module_syncthing.asp", 
-    "md5": "9d72c5165d3a1db2dd39d8b8db66b547", 
+    "md5": "f2fb7d63bec1d620785c105bfb137575", 
     "module": "syncthing", 
     "name": "syncthing", 
     "tar_url": "syncthing/syncthing.tar.gz", 
     "title": "syncthing", 
-    "version": "0.0.1"
+    "version": "0.0.2"
 }

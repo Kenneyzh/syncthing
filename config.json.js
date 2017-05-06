9de00cd4a5532485ceb2e9e7d3fb7ec50fc94054
Kenney <zql254@vip.qq.com>
2017-05-06 10:44:10 +0800

    add into softcenter

---
+++ config.json.js
@@ -0,0 +1,8 @@
+{
+    "description": "多终端同步工具", 
+    "home_url": "Module_syncthing.asp", 
+    "md5": "7210a3db3d3b6400476f7ad4ac06e540", 
+    "module": "syncthing", 
+    "title": "Syncthing", 
+    "version": "0.0.1"
+}
